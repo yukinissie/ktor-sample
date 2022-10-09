ecd73c625958769aebaa374b384afecc35f9f259
yukinissie <bigtree2490@outlook.jp>
2022-10-09 18:33:20 +0900

    try template

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,10 +1,11 @@
 package com.example.plugins
 
-import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.html.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.routing.*
+import kotlinx.html.*
 
 fun Application.configureRouting() {
 
@@ -12,6 +13,44 @@ fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+        get("/html") {
+            val name = "Ktor"
+            call.respondHtml(HttpStatusCode.OK) {
+                head {
+                    title {
+                        +name
+                    }
+                }
+                body {
+                    h1 {
+                        +"Hello from $name!"
+                    }
+                }
+            }
+        }
+        get("/html2") {
+            call.respondHtml {
+                body {
+                    form(
+                        action = "/login",
+                        encType = FormEncType.applicationXWwwFormUrlEncoded,
+                        method = FormMethod.post
+                    ) {
+                        p {
+                            +"Username:"
+                            textInput(name = "username")
+                        }
+                        p {
+                            +"Password:"
+                            passwordInput(name = "password")
+                        }
+                        p {
+                            submitInput() { value = "Login" }
+                        }
+                    }
+                }
+            }
         }
     }
     routing {
